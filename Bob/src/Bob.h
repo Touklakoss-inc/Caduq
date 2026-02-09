5b030edd25c28ef10652154c9649c8e78ef5b395
Arthur Wuhrlin <arthur.wuhrlin@gmail.com>
2026-02-09 22:22:27 +0100

    Add Bob project

---
+++ Bob/src/Bob.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Core.h"
+
+#include "Bob/Log/Log.h"
+#include "Bob/Profile/Instrumentor.h"
--- premake5.lua
+++ premake5.lua
@@ -93,6 +93,11 @@ group ""
         filter "system:windows"
             systemversion "latest"
 
+            defines
+            {
+                "VZ_PLATFORM_WINDOWS"
+            }
+
             links 
             { 
                 "GLFW",
@@ -229,5 +234,57 @@ group ""
 
         filter "configurations:Dist"
             defines "VZ_DIST"
+            runtime "Release"
+            optimize "on"
+
+    project "Bob"
+        location "Bob"
+        kind "StaticLib"
+        language "C++"
+        cppdialect "C++20"
+        staticruntime "on"
+
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp"
+        }
+
+        includedirs
+        {
+            -- Internal files
+            "%{prj.name}/src",
+
+            -- External files
+            "%{IncludeDir.spdlog}"
+        }
+
+        defines
+        {
+            "BOB_BUILD_DLL",
+        }
+
+        filter "system:windows"
+            systemversion "latest"
+
+ 
+        filter { "system:linux", "action:gmake" }
+            buildoptions { "-fpermissive"}
+        
+        filter "configurations:Debug"
+            defines "BOB_DEBUG"
+            runtime "Debug"
+            symbols "on"
+
+        filter "configurations:Release"
+            defines "BOB_RELEASE"
+            runtime "Release"
+            optimize "on"
+
+        filter "configurations:Dist"
+            defines "BOB_DIST"
             runtime "Release"
             optimize "on"
